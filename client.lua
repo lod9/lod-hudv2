88a04027598f00101d04211a714eff66ed0a4e33
Rawe <dortkisibirkanal@gmail.com>
2022-06-13 19:25:46 +0300

    fuel alarm & some basic css edit
    + fuel alarm
    + some basic css edit
    + Minimap check for out of vehicle

--- client.lua
+++ client.lua
@@ -1,86 +1,9 @@
-
+local alarmset = false
 
 local function convertToPercentage(value)
     return math.ceil(value * 10000 - 2001) / 80
     --99,9875
 end
-
--- CreateThread(function()
---   while true do
---     if nuiReady and ESX.PlayerLoaded then
---       local ped = PlayerPedId()
-
---       local curHealth = GetEntityHealth(ped)
---       if curHealth ~= lastHealth then
---         SendMessage('setHealth', { current = curHealth, max = GetEntityMaxHealth(ped) })
---         lastHealth = curHealth
---       end
-
---       local curArmour = GetPedArmour(ped)
---       if curArmour ~= lastArmour then
---         SendMessage('setArmour', curArmour)
---         lastArmour = curArmour
---       end
-
---       if cfg.stamina then
---         local curStamina = GetPlayerStamina(playerId)
---         local maxStamina = GetPlayerMaxStamina(playerId)
---         if curStamina < maxStamina then
---           SendMessage('setStamina', {
---             current = curStamina,
---             max = maxStamina
---           })
---           isResting = false
---         elseif not isResting then
---           SendMessage('setStamina', false)
---           isResting = true
---         end
---       end
-
---       while not maxUnderwaterTime and IsPedSwimmingUnderWater(ped) do
---         ESX.ShowHelpNotification('Initializating HUD... please stay on surface at least 5 seconds!', true)
---         Citizen.Wait(0)
---       end
-
---       if maxUnderwaterTime then
---         local curUnderwaterTime = GetPlayerUnderwaterTimeRemaining(playerId)
---         if curUnderwaterTime < maxUnderwaterTime then
---           SendMessage('setOxygen', {
---             current = curUnderwaterTime,
---             max = maxUnderwaterTime
---           })
---           onSurface = false
---         elseif not onSurface then
---           SendMessage('setOxygen', false)
---           onSurface = true
---         end
---       end
-
---       local inVehicle = IsPedInAnyVehicle(ped, false)
---       if inVehicle then
---         local curVehicle = GetVehiclePedIsUsing(ped, false)
---         SendMessage('setVehicle', {
---           speed = {
---             current = GetEntitySpeed(curVehicle),
---             max = GetVehicleModelMaxSpeed(GetEntityModel(curVehicle))
---           },
---           unitsMultiplier = cfg.metricSystem and 3.6 or 2.236936,
---           fuel = cfg.fuel and GetVehicleFuelLevel(curVehicle),
---         })
---         offVehicle = false
---       elseif not offVehicle then
---         SendMessage('setVehicle', false)
---         offVehicle = true
---       end
---     end
---     Citizen.Wait(cfg.refreshRates.base)
---   end
--- end)
-
-
-
-
-
 
 Citizen.CreateThread(function()
 	while true do
@@ -108,6 +31,13 @@ Citizen.CreateThread(function()
                 farseviye = 0;
             end
         
+            if fuel < 20 then
+                TriggerEvent("benzinuyari")
+            else
+                if fuel < 10 then
+                    TriggerEvent("benzinuyari")
+                end
+            end
             
             SendNUIMessage({
                 carhud = 'arabada';
@@ -127,8 +57,36 @@ Citizen.CreateThread(function()
 	end
 end)
 
+RegisterNetEvent("benzinuyari")
+AddEventHandler("benzinuyari",function()
+    if not alarmset then
+        alarmset = true
+        local i = 5
+        TriggerEvent("DoLongHudText", "Low fuel.",1)
+        while i > 0 do
+            PlaySound(-1, "5_SEC_WARNING", "HUD_MINI_GAME_SOUNDSET", 0, 0, 1)
+            i = i - 1
+            Citizen.Wait(300)
+        end
+        Citizen.Wait(60000)
+        alarmset = false
+    end
+end)
 
+Citizen.CreateThread(function()
+    Citizen.Wait(10000)
+    while true do
+        local ped = PlayerPedId()
+        local sleepThread = 500
 
+        local radarEnabled = IsRadarEnabled()
 
+        if not IsPedInAnyVehicle(ped) and radarEnabled then
+            DisplayRadar(false)
+        elseif IsPedInAnyVehicle(ped) and not radarEnabled then
+            DisplayRadar(true)
+        end
 
-
+        Citizen.Wait(sleepThread)
+    end
+end)
